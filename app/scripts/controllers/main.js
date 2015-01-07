3f58ebf75cbab0a5f70d4fc79541b1f339b15617
Quinn Scripter <qscripter@Quinns-iMac-5K.local>
2015-01-07 00:56:46 -0500

    added ability to sort movies by various values

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,6 +11,11 @@ angular.module('hbogoBrowserApp')
   .controller('MainCtrl', function ($scope, $http) {
     $http.jsonp('http://localhost:5000/catalog.json?callback=JSON_CALLBACK').
       success(function(data) {
+        for (var i=0; i < data.data.length; i++) {
+          data.data[i].endDate = new Date(data.data[i].endDate);
+          data.data[i].startDate = new Date(data.data[i].startDate);
+        }
         $scope.features = data.data;
+        $scope.sort = 'title';
       });
   });
